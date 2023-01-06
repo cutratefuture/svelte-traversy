9871e369ec51fd88ba1c4b4aedac153d585c2221
cutratefuture <christian@cutratefuture.com>
2023-01-05 19:04:21 -0800

    added cards and styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,25 @@
 <script>
-  let firstName = "Christian";
-  let lastName = "Vasquez";
-  let color = "green";
-  let showText = true;
-  let users = [
+  import FeedBackList from "./components/FeedBackList.svelte";
+
+  let feedback = [
     {
-      id: "1",
-      name: "John",
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
     },
     {
-      id: "2",
-      name: "Sara",
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
     },
     {
-      id: "3",
-      name: "Billy",
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
     },
   ];
-
-  $: name = firstName + " " + lastName;
-
-  const toggle = () => {
-    color = color === "blue" ? "red" : "blue";
-    showText = !showText;
-
-	users = [...users, {id: '4', name: 'Jen'}]
-  };
 </script>
 
-<main>
-  <h1 style="color: {color};">Hello {name}!</h1>
-
-  {#if showText}
-    <p>
-      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-      how to build Svelte apps.
-    </p>
-  {:else}
-    <p>No Text</p>
-  {/if}
-
-  <button on:click={toggle}>Click</button>
-
-  {#each users as user (user.id)}
-    <h3>{user.id}: {user.name}</h3>
-  {/each}
+<main class="container">
+  <FeedBackList {feedback}/>
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
---
+++ src/components/FeedBackList.svelte
@@ -0,0 +1,8 @@
+<script>
+  import FeedBackItem from "./FeedBackItem.svelte";
+  export let feedback = [];
+</script>
+
+{#each feedback as fb (fb.id)}
+  <FeedBackItem item={fb} />
+{/each}
