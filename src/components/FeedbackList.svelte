0dd4fe03efc7c831f86057ce48466e9771fa4f18
cutratefuture <christian@cutratefuture.com>
2023-01-05 21:49:56 -0800

    animations

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,12 @@
+
 <script>
-  import FeedbackItem from "./FeedbackItem.svelte";
-  export let feedback = [];
+  import { fade, scale } from 'svelte/transition'
+  import FeedbackItem from './FeedbackItem.svelte'
+  export let feedback = []
 </script>
 
 {#each feedback as fb (fb.id)}
+<div in:scale out:fade="{{ duration: 500 }}">
   <FeedbackItem item={fb} on:delete-feedback />
+</div>
 {/each}
